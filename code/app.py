b14f6c1ba4483ef88d9d63581c5a781b77a7a746
Putter <chantapat@protonmail.com>
2021-01-10 19:32:06 +0700

    Stopped using absolute file paths to fix deployment errors

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+from db import db
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +10,8 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'sqlite:////Code/python/sql-alchemy/data.db')
+    'sqlite:///data.db')
+    # 'sqlite:////Code/python/sql-alchemy/data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "somekey"
 api = Api(app)
@@ -23,6 +25,14 @@ api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 
+
+
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
