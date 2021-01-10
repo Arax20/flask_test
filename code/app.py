2561b91bda87d3a1a3709fcf31dc03180544761f
Putter <chantapat@protonmail.com>
2021-01-10 19:50:14 +0700

    Added PostgreSQL support

--- code/app.py
+++ code/app.py
@@ -1,3 +1,5 @@
+import os
+
 from db import db
 from flask import Flask
 from flask_restful import Api
@@ -9,9 +11,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'sqlite:///data.db')
-    # 'sqlite:////Code/python/sql-alchemy/data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
+# 'sqlite:////Code/python/sql-alchemy/data.db') LOCAL ABSOLUTE path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "somekey"
 api = Api(app)
@@ -26,13 +28,13 @@ api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 
 
-
 db.init_app(app)
 
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
